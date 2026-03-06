f21d28dcbf41b110a6667a52c21657acc77308cf
ST10506431 <ST10506431@myemeris.edu.za>
2026-03-06 10:50:16 +0200

    Init commit

---
+++ mavenproject2BMICalculator/src/main/java/com/mycompany/mavenproject2bmicalculator/Mavenproject2BMICalculator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2bmicalculator;
+
+/**
+ *
+ * @author hazel
+ */
+
+/*
+Activity 5: BMI Calculator
+Objective: Learn how to use division and multiplication in a real-world formula.
+Steps to Follow:
+1. Create a new Java project called BMICalculator.
+2. Import the Scanner class at the beginning.
+3. Inside the main method, do the following:
+o Ask the user to enter:
+▪ Weight in kilograms (kg)
+▪ Height in meters (m)
+o Use the formula:
+BMI = weight / (height × height)
+o Divide the weight by the square of the height.
+o Display the BMI value.
+*/
+public class Mavenproject2BMICalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
